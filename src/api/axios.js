053f4740955c6af4f242b80fd4cda151c5bc7151
tmpelay <tmpelay@gmail.com>
2024-11-13 14:03:54 -0300

    Change API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: " https://linces-voley-sales-back.onrender.com/api", // https://linces-voley-sales-back.onrender.com/api --- http://localhost:8080/api
+  baseURL: "https://linces-voley-sales-back-production.up.railway.app/api", // https://linces-voley-sales-back-production.up.railway.app/api --- http://localhost:8080/api
   withCredentials: true,
 });
 
