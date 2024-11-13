ffe95670938d559423cb990bb5f100248de47177
tmpelay <tmpelay@gmail.com>
2024-11-13 12:31:55 -0300

    Fix API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api", // https://linces-voley-sales-back.onrender.com/api
+  baseURL: " https://linces-voley-sales-back.onrender.com/api", // https://linces-voley-sales-back.onrender.com/api --- http://localhost:8080/api
   withCredentials: true,
 });
 
